386676acb20b54e142e5881404ddef239e681f92
Ben <burge337@morris.umn.edu>
2020-09-29 11:47:57 -0500

    Get disemvowel mostly working for small inputs.
    It currently doesn't pass the stdin stout test because it returns a bunch of null characters
    for any part of the buffer size that doesn't have text written to it.
    
    Going forward we need to put our read and write statements in a while loop, so we can process more than 1 buffer
    full of text from a file. As it stands, we would be unable to process really long files.
    
    We also need to process files provided in the command line as well, but that will come later.

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -1,20 +1,22 @@
-#include <stdio.h>
-#include <stdbool.h>
+#include "file_disemvowel.h"
 
 #define BUF_SIZE 1024
 
 bool is_vowel(char c) {
 	if(c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U'){
 		return true;
+
 	}
 	return false;
 }
 
 int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
+	// number of non-vowels
+	int nv_count = 0;
 	for(int i =0; i < num_chars; i++){
-		int nv_count = 0;
+		// counter for out_buf
 		if(!is_vowel(in_buf[i])){
-			out_buf[i] = in_buf[i];
+			out_buf[nv_count] = in_buf[i];
 			nv_count++;
 		}	
 	}
@@ -23,24 +25,22 @@ return nv_count;
 }
 
 void disemvowel(FILE* inputFile, FILE* outputFile) {
-	in_file = fopen("inputFile", "r");
-	out_file = fopen("outputFile", "w");
-	char* input_buf[BUF_SIZE];
-	char* output_buf[BUF_SIZE};
+	char* input_buf = (char*)calloc(BUF_SIZE, 1);
+	char* output_buf = (char*)calloc(BUF_SIZE, 1);
 	//Empty space is length of inputFile
-	fread(input_buf, sizeOf(char*), BUF_SIZE ,in_file);
-	fread(output_buf, sizeOf(char*), BUF_SIZE, out_file);
-	copy_non_vowels(num, input_buf, output_buf);
-	fclose(in_file);
-	fclose(out_file);
+	fread(input_buf, 1, BUF_SIZE ,inputFile);
+	copy_non_vowels(BUF_SIZE, input_buf, output_buf);
+	fwrite(output_buf, 1, BUF_SIZE, outputFile);
 
 }
 
 int main(int argc, char *argv[]) {
-	FILE *inputFile;
-	FILE *outputFile;
+	FILE *inputFile = stdin;
+	FILE *outputFile = stdout;
 
 	disemvowel(inputFile, outputFile);
+	fclose(inputFile);
+	fclose(outputFile);
 
 	return 0;
 }
