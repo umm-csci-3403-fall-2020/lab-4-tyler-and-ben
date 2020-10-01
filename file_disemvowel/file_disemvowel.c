e9833f37d22ee58ca0809f60e652acbf93c26217
Ben <burge337@morris.umn.edu>
2020-10-01 11:25:21 -0500

    Finish implementation of disemvowel_file.

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -1,7 +1,7 @@
 #include "file_disemvowel.h"
 
 #define BUF_SIZE 1024
-
+// returns true if the character passed is a vowel
 bool is_vowel(char c) {
 	if(c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U'){
 		return true;
@@ -20,27 +20,46 @@ int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
 			nv_count++;
 		}	
 	}
-free(in_buf);
 return nv_count;
 }
 
 void disemvowel(FILE* inputFile, FILE* outputFile) {
 	char* input_buf = (char*)calloc(BUF_SIZE, 1);
 	char* output_buf = (char*)calloc(BUF_SIZE, 1);
-	//Empty space is length of inputFile
+	// while fread still has more stuff to read through
+	while((inputFile != NULL && outputFile!=NULL) && fread(input_buf, 1, BUF_SIZE ,inputFile) > 0){
 	fread(input_buf, 1, BUF_SIZE ,inputFile);
 	copy_non_vowels(BUF_SIZE, input_buf, output_buf);
-	fwrite(output_buf, 1, BUF_SIZE, outputFile);
-
+	fwrite(output_buf, 1, strlen(output_buf), outputFile);
+	}
+	free(input_buf);
+	free(output_buf);
 }
 
 int main(int argc, char *argv[]) {
-	FILE *inputFile = stdin;
-	FILE *outputFile = stdout;
-
+	FILE* inputFile = NULL;
+	FILE* outputFile = NULL;
+	// if we also receive an input file
+	if(argc == 2){
+		inputFile = fopen(argv[1], "r");
+		outputFile = stdout;
+	}
+	// if we receive an input and output file
+	else if(argc == 3){
+		inputFile = fopen(argv[1], "r");
+		outputFile = fopen(argv[2], "w");
+	}
+	else{
+		inputFile = stdin;
+		outputFile = stdout;
+	}
 	disemvowel(inputFile, outputFile);
-	fclose(inputFile);
+	if(inputFile!=NULL){
+		fclose(inputFile);
+	}
+	if(outputFile!=NULL){
 	fclose(outputFile);
+	}
 
 	return 0;
 }
--- file_disemvowel/file_disemvowel.h
+++ file_disemvowel/file_disemvowel.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <cstring>
 
 bool is_vowel(char c);
 int copy_non_vowels(int num_chars, char* in_buf, char* out_buf);
