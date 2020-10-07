2067c11095e9a6854a89b942f0972f18dfcb9d4e
Ben <burge337@morris.umn.edu>
2020-10-06 21:10:25 -0500

    Fix shellcheck whining
    Just needed to double quote a couple variables to prevent word globbing.

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -4,6 +4,6 @@ path="$1"
 file_count="$(find "$path" -type f -print | wc -l)"
 dir_count="$(find "$path" -type d -print | wc -l)"
 
-echo "There were "$dir_count" directories."
-echo "There were "$file_count" regular files."
+echo "There were ""$dir_count"" directories."
+echo "There were ""$file_count"" regular files."
 
